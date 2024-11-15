be9eff904acfb1e9fea12840277acf7b95e04380
ShunPractice <lawunshunlett@gmail.com>
2024-11-15 20:28:12 +0800

    bmi is finished

---
+++ Calculate_bmi.py
@@ -0,0 +1,20 @@
+def calculate_bmi():
+    height=float(input('Enter height:'))
+    weight=float(input('Enter weight:'))
+    BMI= weight/height*height
+    print("BMI is "+str(BMI))
+    if(BMI<18.5):
+        print("Under Weight")
+    elif (18.5<=BMI<=25.0):
+        print("BMI is normal weight")
+    elif(BMI>25.0):
+        print("Over weight")
+    else:
+        print('Invalid')
+
+#calculate_bmi(weight=57,height=1.73)
+
+
+
+if __name__=='__main__':#put space between if and __ and must be 'main'
+    calculate_bmi()
